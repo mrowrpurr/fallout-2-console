a5c73f051a3468201eb17ceb3c16089482ca950f
Mrowr Purr <mrowr.purr@gmail.com>
2022-07-12 00:47:10 -0700

    'Enter' clears command entry and can print white text

--- scripts/headers/Console.h
+++ scripts/headers/Console.h
@@ -38,15 +38,44 @@
 #define get_console_keymap_array             get_sfall_global_int(CONSOLE_KEYMAP_GLOBAL_SFALL_VAR_NAME)
 #define is_console_initialized               (get_console_data != 0)
 
-procedure ConsoleShow begin
+// TODO make this a macro
+procedure ConsolePrint(variable text) begin
+    variable console_data = get_console_data_array;
+    if console_data then begin
+        call array_push(console_data.display_text_lines, text);
+    end else
+        debug_msg("Cannot print to console, not initialized yet!");
+end
+
+// procedure ConsoleSprintf(variable text, variable arg)
+
+procedure ConsoleExecuteCommand(variable command) begin
+    display_msg("EXEC " + command);
+    call ConsolePrint("I printed this! from exec");
+    call ConsolePrint("I printed this too! from exec");
+end
+
+procedure ConsoleUI_Show begin
+    display_msg("SHOW!");
+
     variable console_data = get_console_data_array;
 
     if not console_data then begin
-        console_data = { "text": "", "visible": false, "initialized": false };
+        console_data = {
+            "command_text": "",
+            "visible": false,
+            "initialized": false
+        };
         set_sfall_global(CONSOLE_DATA_GLOBAL_SFALL_VAR_NAME, console_data);
         fix_array(console_data);
-        console_data.history = [];
-        fix_array(console_data.history);
+        
+        // History of commands. To be saved in .ini and can get to with UP (and we'll make CTRL-R eventually) 
+        console_data.command_history = [];
+        fix_array(console_data.command_history);
+        
+        // The text display output where commands print their output (in white)
+        console_data.display_text_lines = [];
+        fix_array(console_data.display_text_lines);
 
         variable keymap = {};
         fix_array(keymap);
@@ -79,7 +108,7 @@ procedure ConsoleShow begin
     end
 end
 
-procedure ConsoleHide begin
+procedure ConsoleUI_Hide begin
     variable console_data = get_console_data_array;
     if console_data and console_data.visible then begin
         console_data.visible = false;
@@ -87,33 +116,60 @@ procedure ConsoleHide begin
     end
 end
 
-procedure ConsoleToggleOpen begin
+procedure ConsoleUI_ToggleOpen begin
     variable console_data = get_console_data_array;
     if console_data then begin
-        if console_data.visible then call ConsoleHide;
-        else call ConsoleShow;
+        if console_data.visible then call ConsoleUI_Hide;
+        else call ConsoleUI_Show;
     end else
-        call ConsoleShow;
+        call ConsoleUI_Show;
 end
 
-procedure ConsoleRefresh begin
+procedure ConsoleUI_Refresh begin
     variable console_data = get_console_data_array;
     if console_data and console_data.visible then begin
         hide_window(CONSOLE_WINDOW_NAME);
         SelectWin(CONSOLE_WINDOW_NAME);
         draw_image(CONSOLE_BACKGROUND_FRM, 0, 0, 0, true);
         SetFont(CONSOLE_TEXT_FONT);
-        SetTextColor(0.0, 1.0, 0.0); // Green
+
+        // Green
+        SetTextColor(0.0, 1.0, 0.0);
         Format("$", 46, 40, 300, 12, justifyleft);
-        Format(console_data.text, 57, 40, 300, 12, justifyleft);
+        Format(console_data.command_text, 57, 40, 300, 12, justifyleft);
+        
+        // White
+        if len_array(console_data.display_text_lines) > 0 then begin
+            SetTextColor(1.0, 1.0, 1.0);
+            variable line_height = 14;
+            variable height = 55;
+            variable line;
+            foreach line in (console_data.display_text_lines) begin
+                Format(line, 46, height, 300, 12, justifyleft);
+                height += line_height;
+            end
+        end
+        
         show_window(CONSOLE_WINDOW_NAME);
     end
 end
 
-procedure ConsoleTypeText(variable character) begin
+procedure ConsoleUI_Print(variable text) begin
+    display_msg("CONSOLE UI PRINT...");
+end
+
+procedure ConsoleUI_ExecuteCurrentCommand begin
+    variable console_data = get_console_data_array;
+    variable command = console_data.command_text;
+    console_data.command_text = "";
+    call ConsoleExecuteCommand(command);
+    call ConsoleUI_Refresh;
+end
+
+procedure ConsoleUI_TypeText(variable character) begin
     variable console_data = get_console_data_array;
     if console_data and console_data.visible then begin
-        console_data.text += character;
-        call ConsoleRefresh;
+        console_data.command_text += character;
+        call ConsoleUI_Refresh;
     end
 end
