17f4c1a7c9d2b4735a9d6a60b58e8d1aac3f5acb
Mrowr Purr <mrowr.purr@gmail.com>
2022-07-18 06:35:38 -0700

    Fix clicking on targets (right-click instead of left)

--- scripts/headers/Console/Internal/ConsoleRuntimeMouseBindings.h
+++ scripts/headers/Console/Internal/ConsoleRuntimeMouseBindings.h
@@ -3,11 +3,10 @@
 // int arg0 - event type: 1 - pressed, 0 - released
 // int arg1 - button number (0 - left, 1 - right, up to 7)
 procedure ConsoleRuntime_Hooks_MouseClick begin
-    variable pressed    = get_sfall_arg,
-             left_click = (get_sfall_arg == 0);
-    if pressed and left_click and is_console_open then begin
+    variable pressed     = get_sfall_arg,
+             right_click = (get_sfall_arg == 1);
+    if pressed and right_click and is_console_open then begin
         variable target;
-        debug_msg("CLICK!");
         if key_pressed(KEYCODE_LEFT_SHIFT) or key_pressed(KEYCODE_RIGHT_SHIFT) then // Tile
             target = tile_under_cursor;
         else if key_pressed(KEYCODE_LEFT_CTRL) or key_pressed(KEYCODE_RIGHT_CTRL) then // Critters only
